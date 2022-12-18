ee9f6ebfe60e3f2fcac3728d88f830574420d9e9
Abbas Lutvaliyev <91781891+z3ven@users.noreply.github.com>
2022-12-18 10:03:23 +0400

    Add files via upload

---
+++ RetroBrowsing/localization/en_us/about.py
@@ -0,0 +1,2 @@
+NAME = "RetroBrowsing 1.0 on English"
+DESC = "The browser is developed in Python and distributed freely. Supports modern sites using the http protocol. You can learn more on project resources such as Github. This browser was developed to be presented at hour of code 2022 in Azerbaijan."
---
+++ RetroBrowsing/localization/en_us/main.py
@@ -0,0 +1,3 @@
+WELCOME = "Welcome to the RetroBrowsing. To open the website enter the addres name without http://"
+GOBTN  = "Go"
+ABOUTWINDOW = "About"
