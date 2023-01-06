00bdad9b719c747bdf24d3f19100fa265f15561c
Abbas Lutvaliyev <91781891+z3ven@users.noreply.github.com>
2023-01-06 21:08:51 +0400

    Delete localization directory

--- localization/ru_ru/main.py
+++
@@ -1,4 +0,0 @@
-WELCOME = "Добро пожаловать в RetroBrowsing 1.0. Для того чтобы открыть сайт введите его адрес в строку ввода без http://. Откроется вкладка."
-GOBTN  = "Перейти на веб сайт."
-ABOUTWINDOW = "О программе"
-FASTGO = {"GOOGLE": "http://google.com", "RUTUBE": "https://rutube.ru", "YAPPY": "https://yappi.media", "VK": "http://vk.com", "ROSGRAM": "http://rosgram.ru"}
