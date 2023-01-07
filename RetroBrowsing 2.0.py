52808c6fad1826f99d1a93753354c683d1199443
Abbas Lutvaliyev <91781891+z3ven@users.noreply.github.com>
2023-01-07 12:33:33 +0400

    2.0 Update Files
    Update.

---
+++ RetroBrowsing 2.0.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import webview
+import config
+from flask import Flask, render_template, request, redirect
+
+
+
+
+
+guifile = "../static/default.css"
+if config.THEME != "DEFAULT":
+    guifile = f"../static/{config.THEME}"
+
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    static_folder = os.path.join(sys._MEIPASS, 'static')
+    server = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+else:
+     server = Flask(__name__)
+
+
+
+@server.route("/gui/<lang>")
+def gui(lang):
+    if config.LOCALIZATION == "en_us":
+        return render_template("gui.html", themefile=guifile)
+    elif config.LOCALIZATION == "ru_ru":
+        return render_template("gui_ru.html", themefile=guifile)
+@server.route('/gui/<lang>', methods=['POST'])
+def guiurl(lang):
+    requestuserurl = request.form['text']
+    userurl = str(requestuserurl)
+    if 'file://' in userurl:
+        path = userurl.split("file://")[1]
+        return render_template("openfile.html", file=path)
+    else:
+        return redirect(f"http://{userurl}")
+@server.route("/update/<lang>")
+def update(lang):
+    if config.LOCALIZATION == "en_us":
+        return render_template("update-en.html")
+    elif config.LOCALIZATION == "ru_ru":
+        return render_template("update-ru.html")
+@server.route("/about/<lang>")
+def about(lang):
+    if config.LOCALIZATION == "en_us":
+        return render_template("about-en.html", language=config.LOCALIZATION, build=config.VERSION, theme=config.THEME, retroengine="RetroEngine 2.0, GUI=HTML, CURRENTSTATE=RENDERING", themefile=guifile)
+    elif config.LOCALIZATION == "ru_ru":
+        return render_template("about-ru.html", language=config.LOCALIZATION, build=config.VERSION, theme=config.THEME, retroengine="RetroEngine 2.0, GUI=HTML, CURRENTSTATE=RENDERING", themefile=guifile)
+
+
+def serverrun():
+    server.run(host="0.0.0.0", port=2386)
+retrobrowsing = webview.create_window(title="RetroBrowsing 2.0", url=f"http://127.0.0.1:2386/gui/{config.LOCALIZATION}", min_size=(800, 600))
+webview.start(serverrun)
+
---
+++ config.py
@@ -0,0 +1,5 @@
+WEB_ENGINE = "DEFAULT" # If the "DEFAULT" it will use the default engine on your OS.
+VERSION = "2.0 Beta Previev" # version
+THEME = "DEFAULT" # working now
+LOCALIZATION = "en_us" # language of your browser
+
